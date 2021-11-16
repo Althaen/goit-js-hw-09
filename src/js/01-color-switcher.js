b97bb63a6ef6869a21f7f8bd51d00103ffb93fbd
Althaen <Evgenius16.k@gmail.com>
2021-11-17 02:12:51 +0300

    Добавляет реализацию первого задания

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onColorRandomizerStart() {
+  startButton.setAttribute('disabled', true);
+  stopButton.removeAttribute('disabled');
+
+  randomizerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onColorRandomizerStop() {
+  startButton.removeAttribute('disabled');
+  stopButton.setAttribute('disabled', true);
+  clearInterval(randomizerID);
+}
+
+stopButton.setAttribute('disabled', true);
+
+startButton.addEventListener("click", onColorRandomizerStart);
+stopButton.addEventListener("click", onColorRandomizerStop);
 
